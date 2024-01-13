4fa4bc4589c600c532fbbeec9170635a3aeec14b
Savita patidar <104501069+Sv567@users.noreply.github.com>
2024-01-13 22:40:28 +0530

    Update script.js

--- togglebtn/script.js
+++ togglebtn/script.js
@@ -1,4 +1,28 @@
-const toggleBtn = document.querySelector(".toogle-icon");
-const circle = document.querySelector(".toggle-tbn");
-toggleBtn.addEventListener("click",()=>{                                                     
-});
+let body = document.querySelector("body");
+let btn = document.querySelector(".btn");
+let heading = document.querySelector("h1");
+let circle = document.querySelector(".circle");
+circle.style.backgroundColor = "white";
+btn.style.backgroundColor = "black";
+
+let toggle = false;
+
+btn.addEventListener("click", () => {
+    if(!toggle){
+        circle.classList.add("dark-mode");
+        circle.style.backgroundColor = "black";
+        btn.style.backgroundColor = "white";
+        body.style.backgroundColor = "black";
+        heading.style.color = "white";
+        toggle = true;
+    }
+    else{
+        circle.classList.remove("dark-mode");
+        circle.style.backgroundColor = "white";
+        body.style.backgroundColor = "white";
+        btn.style.backgroundColor = "black";
+        heading.style.color = "black";
+        toggle = false;
+    }
+
+})
